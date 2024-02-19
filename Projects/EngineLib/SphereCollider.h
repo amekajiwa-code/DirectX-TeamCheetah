822ea258e43544390be0233ea67e5638e353298b
TrueCircular <OriJin0322@gmail.com>
2024-02-20 02:52:05 +0900

    save

--- Projects/EngineLib/SphereCollider.h
+++ Projects/EngineLib/SphereCollider.h
@@ -8,7 +8,7 @@ public:
 	SphereCollider();
 	virtual ~SphereCollider();
 private:
-	float			_radius = 1.f;
+	float			_radius = 0.5f;
 	BoundingSphere	_boundingSphere;
 	Vec3			_sphereWeight = Vec3(0.f);
 public:
