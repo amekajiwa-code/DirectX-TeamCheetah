d7ab334961ac047093f202b43bc98d4ceaea7ecd
동현 김 <roskn142@naver.com>
2024-02-25 16:07:50 +0900

    com

--- Libraries/Include/engine/Sounds.h
+++ Libraries/Include/engine/Sounds.h
@@ -3,25 +3,22 @@
 #include "fmod\fmod.hpp"
 #include "fmod\fmod_errors.h"
 
-//class Sound : public ResourceBase
-//{
-//	using Super = ResourceBase;
-//
-//	//need each n files to create;
-//	FMOD::Sound* fm_Sound;
-//
-//public:
-//	Sound();
-//	~Sound();
-//public:
-//	bool Init();
-//	bool Frame();
-//	void Load(const wstring& path);
-//	bool Release();
-//	bool Pause();
-//	bool Resume();
-//	FMOD::Channel Play(bool doLoop = false);
-//	FMOD::Channel PlayEffect();
-//	bool Stop();
-//	bool Volume(float vol);
-//};
+class Sound : public ResourceBase
+{
+	using Super = ResourceBase;
+
+	//need each n files to create;
+	FMOD::Sound* fm_Sound;
+	float volume=100;
+public:
+	Sound();
+	~Sound();
+public:
+	void Load(const wstring& path);
+	FMOD::Channel* Play(bool doLoop);
+	FMOD::Channel* Play3D(bool doLoop, Vec3 pos, Vec3 vel);
+	void PlayEffect();
+	void Play3DEffect(Vec3 pos, Vec3 vel);
+	
+	void SetVolume(float vol) {volume = vol;};
+};
